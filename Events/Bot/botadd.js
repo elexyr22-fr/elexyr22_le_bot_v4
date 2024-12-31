cfd88a9ca94f8c3ecf6c0a46566078a517ecb412
Elexyr22 👑 <pro@elexyr22.fr>
2024-12-31 16:10:46 +0100

    Update botadd.js

--- Events/Bot/botadd.js
+++ Events/Bot/botadd.js
@@ -1,25 +1,37 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 const Event = require("../../Structure/Event");
 
-module.exports = new Event("guildCreate", async (bot, guild, user) => {
+module.exports = new Event("guildCreate", async (bot, guild) => {
+    console.log(`Nouveau serveur : ${guild.name} je suis à  ${bot.guilds.cache.size} serveurs !`);
 
-console.log(`New serveur : ${guild.name} je suis à  ${bot.guilds.cache.size} serveur !`)
-    
-let serveur = guild.channels.cache.find(channel => channel.type === "GUILD_TEXT" && channel.permissionsFor(bot.user.id).has("CREATE_INSTANT_INVITE"))
-if(!serveur) return;
+    let serveur = guild.channels.cache.find(channel => channel.type === "GUILD_TEXT" && channel.permissionsFor(bot.user.id).has("CREATE_INSTANT_INVITE"));
+    if (!serveur) return;
 
-let invite = await serveur.createInvite({ maxAge: 0, maxUses: 0 })
-    
-      const channel = bot.channels.cache.get("ID")  
-      let addembed = new Discord.MessageEmbed()
+    let invite = await serveur.createInvite({ maxAge: 0, maxUses: 0 });
+
+    const channelId = "ID_CHANNEL"; 
+    let channel = bot.channels.cache.get(channelId);
+
+    if (!channel) {
+        try {
+            channel = await bot.channels.fetch(channelId);
+        } catch (error) {
+            return;
+        }}
+
+    let addembed = new Discord.MessageEmbed()
         .setTitle(`Join de : ${guild.name} *(${guild.id})*`)
         .setThumbnail(guild.iconURL())
         .addField(`👑 Propriétaire:`, `<@${guild.ownerId}> - *(${guild.ownerId})*`)
         .addField(`Nombre de membres:`, `${guild.memberCount}`)
-      	.addField(`🛬 Invitation :`, `${invite}`)
+        .addField(`🛬 Invitation :`, `${invite}`)
         .setColor("11d646")
         .setTimestamp()
-        .setFooter(`Merci grâce à toi nous sommes à ${bot.guilds.cache.size} serveurs`, bot.user.displayAvatarURL())
-      channel.send({embeds : [addembed]})
-    })
+        .setFooter(`Merci grâce à toi nous sommes à ${bot.guilds.cache.size} serveurs`, bot.user.displayAvatarURL());
 
+    if (channel) {
+        channel.send({ embeds: [addembed] });
+    } else {
+        return;
+    }
+});
